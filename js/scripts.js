95c8364d57cb40d356192fc04628bba7524b59bb
Galin McMahon and James Higgins <galinmcmahon@gmail.com>
2017-03-22 13:55:44 -0700

    Create functionality for confirmation page upon entering appointment information.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,23 @@
 $(document).ready(function() {
+   $(".btn").click(function() {
+    $(".container").hide();
+   $(".confirmation").show();
+});
+
   $("#formOne").submit(function(event) {
     var yourNameInput = $("input#yourName").val();
+    var descInput = $("input#desc").val();
+    var dateInput = $("input#date").val();
+    var timeInput = $("input#time").val();
 
     $(".yourName").text(yourNameInput);
+    $(".desc").text(descInput);
+    $(".date").text(dateInput);
+    $(".time").text(timeInput);
 
-    $("#letter").show();
+    // $("#letter").show();
 
+    // event.preventDefault();
     event.preventDefault();
   });
 });
